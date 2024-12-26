9bd7b05031af91372afa834a0fce12b202f01e21
rafatb <rafatbl@gmail.com>
2024-12-26 11:32:35 +0200

    my weather app

--- weather/main.py
+++ weather/main.py
@@ -1,48 +1,29 @@
-import streamlit as st
 from weather_utility import *
-import datetime
-import plotly.express as px
 
-# Function to display 3-hour interval temperature chart
-def display_forecast_chart(weather_data):
-    if weather_data and 'list' in weather_data:
-        forecast_list = weather_data['list'][:24]  # Limit to 24 intervals (3-hour each)
-        times = []
-        temperatures = []
-
-        for forecast in forecast_list:
-            timestamp = forecast['dt']
-            temp = forecast['main']['temp']
-
-            time = datetime.datetime.fromtimestamp(timestamp)
-
-            times.append(time)
-            temperatures.append(temp)
-
-        # Create a line chart using Plotly
-        fig = px.line(x=times, y=temperatures, labels={'x': 'Time', 'y': 'Temperature (°C)'}, title='3-Hour Interval Temperature Forecast')
-        plotlyContainer = st.container(border=True)
-        plotlyContainer.plotly_chart(fig)
 
 def main():
     # Title of the app
     headerContainer = st.container(border=True)
-    headerContainer.title("My Weather App")
+    headerContainer.title("Welcome to my Weather App")
     imageCointainer = st.container(border=True)
     imageCointainer.image("data/weather5.png")
 
     # Ask the user for the city
     inputContainer = st.container(border=True)
     city = inputContainer.text_input("Please enter a city name to get the weather data", "")
+
     api_key = st.secrets["api_key"]
 
 
     if city:
         try:
+            # get current weather detail
             weather_data = get_weather(city, api_key)
+
+            # get city coordinates
             lat, lon = get_city_coordinates(city, api_key)
 
-            # Extract relevant data
+            # Extract current weather relevant data
             main = weather_data["main"]
             weather = weather_data["weather"][0]
             temperature = main["temp"]
@@ -50,7 +31,7 @@ def main():
             description = weather["description"]
             icon = weather["icon"]
 
-            # Display weather info
+            # Display current weather info
             resultContainer = st.container(border=True)
             resultContainer.subheader(f"Weather in {city.capitalize()} , lat={lat} , lon={lon}:")
             resultContainer.write(f"**Temperature**: {temperature}°C")
@@ -61,8 +42,11 @@ def main():
             icon_url = f"http://openweathermap.org/img/wn/{icon}.png"
             resultContainer.image(icon_url, width=100)
 
+            # get forcast fopr the next 3 days
             weather_data = get_forecast_data_v25(city, api_key)
             if weather_data:
+
+                # display the 3 days forcast as line graph
                 display_forecast_chart(weather_data)
         except Exception as e :
             match str(e):
@@ -72,8 +56,5 @@ def main():
                     st.error(f'Could not retrieve weather data for {city},  City not exists, \n\n Response Code {str(e)}  ')
                 case _:
                     st.error(f'Could not retrieve weather data for {city}, \n\n Response Code {str(e)}  ')
-    # else:
-    #     st.info("Please enter a city name to get the weather data.")
-
 if __name__ == "__main__":
     main()
--- weather/weather_utility.py
+++ weather/weather_utility.py
@@ -1,6 +1,10 @@
 import requests
+import datetime
+import plotly.express as px
+import streamlit as st
 
-# Function to fetch weather data
+
+# Function to fetch the current weather data
 def get_weather(city, api_key):
     base_url = "http://api.openweathermap.org/data/2.5/weather"
     # Construct the URL with the city and API key
@@ -15,7 +19,7 @@ def get_weather(city, api_key):
     else:
         raise Exception(response.status_code)
 
-
+# fetch the next 3 days forcast with 3 hours intervals
 def get_forecast_data_v25(city, api_key):
     url = f'http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric'
     try:
@@ -27,7 +31,7 @@ def get_forecast_data_v25(city, api_key):
     except Exception as err:
         raise Exception(f"Error occurred: {err}")
 
-
+# get city coordinates
 def get_city_coordinates(city_name, api_key):
     geo_url = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
     geo_response = requests.get(geo_url)
@@ -35,3 +39,24 @@ def get_city_coordinates(city_name, api_key):
         location = geo_response.json()[0]
         return location['lat'], location['lon']
     return None, None
+
+# Function to display 3-hour interval temperature chart for the next 3 days
+def display_forecast_chart(weather_data):
+    if weather_data and 'list' in weather_data:
+        forecast_list = weather_data['list'][:24]  # Limit to 24 intervals (3-hour each)
+        times = []
+        temperatures = []
+
+        for forecast in forecast_list:
+            timestamp = forecast['dt']
+            temp = forecast['main']['temp']
+
+            time = datetime.datetime.fromtimestamp(timestamp)
+
+            times.append(time)
+            temperatures.append(temp)
+
+        # Create a line chart using Plotly
+        fig = px.line(x=times, y=temperatures, labels={'x': 'Time', 'y': 'Temperature (°C)'}, title='3-Hour Interval Temperature Forecast')
+        plotlyContainer = st.container(border=True)
+        plotlyContainer.plotly_chart(fig)
